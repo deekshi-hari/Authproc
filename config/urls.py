77684e7ad5dc3a36ecbb170383db55e451504316
tradebrains <deekshith.hari@tradebrains.in>
2021-11-23 18:25:37 +0530

    settngd

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views import static
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),
     path('accounts/', include('allauth.urls')),
-    path('auth/', include('rest_auth.urls'))
+    path('auth/', include('rest_auth.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
