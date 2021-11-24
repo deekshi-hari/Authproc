ae36ab6faa7b881183e970e12f350888fe2d5f72
tradebrains <deekshith.hari@tradebrains.in>
2021-11-24 18:56:06 +0530

    apple3 auth rest

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,13 +3,16 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 from . import views
 from django.views.generic import TemplateView
-# from .views import GoogleLogin
+from .views import GoogleLogin
+from .views import AppleLogin
+
 
 urlpatterns = [
     path('', views.login, name="login"),
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    # path('auth/google/', GoogleLogin.as_view(), name='google_login')
+    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('auth/apple/', AppleLogin.as_view(), name='google_login')
 ]
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -5,8 +5,10 @@ from rest_framework.permissions import IsAuthenticated
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_auth.registration.views import SocialLoginView
-
-
+from allauth.socialaccount.providers.apple.views import AppleOAuth2Adapter
+from allauth.socialaccount.providers.apple.client import AppleOAuth2Client
+from rest_auth.registration.views import SocialLoginView
+from .serializers import CustomAppleSocialLoginSerializer
 
 # Create your views here.
 def login(request):
@@ -21,8 +23,13 @@ class HelloView(APIView):
         content = {'message': 'Hello, World!'}
         return Response(content)
 
-# class GoogleLogin(SocialLoginView):
-#     adapter_class = GoogleOAuth2Adapter
-#     client_class = OAuth2Client
-#     callback_url = "http://127.0.0.1:8000/api/auth/google/callback/"
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = "http://127.0.0.1:8000/api/auth/google/callback/"
 
+class AppleLogin(SocialLoginView):
+    adapter_class = AppleOAuth2Adapter
+    callback_url = 'http://127.0.0.1:8000/api/auth/apple/callback/"'
+    client_class = AppleOAuth2Client
+    serializer_class = CustomAppleSocialLoginSerializer
